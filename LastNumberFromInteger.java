ede31a3084fd09852f5034ba0c6137daa3d1636d
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2023-08-04 16:09:53 +0530

    Create LastNumberFromInteger.java

---
+++ LastNumberFromInteger.java
@@ -0,0 +1,14 @@
+package com.bean;
+
+public class LastNumberFromInteger {
+
+	public static void main (String [] args) {
+		
+		int number=19023832;
+		System.out.println(String.valueOf(number).substring((String.valueOf(number).length()-1)));
+		
+			}
+		
+	}
+	
+	
